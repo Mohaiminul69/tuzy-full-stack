f8471b82c9f5a9eb7a11c3aebe575f45b4fc89a8
Mohaiminul69 <mohaiminul69@gmail.com>
2025-05-21 22:34:42 +0600

    reivew changes.

--- backend/db/migrate/20250428154218_add_colum_to_booking.rb
+++
@@ -1,8 +0,0 @@
-class AddColumToBooking < ActiveRecord::Migration[8.0]
-  def change
-    add_column :bookings, :phone_number, :string, null: false
-    add_column :bookings, :address, :string, null: false
-    add_column :bookings, :credit_card_number, :string, null: false
-    add_column :bookings, :booking_date, :date, null: false
-  end
-end
