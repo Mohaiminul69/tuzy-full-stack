ee43a3f22d38d9a231141139db83199b4df9c44f
Mohaiminul69 <mohaiminul69@gmail.com>
2025-06-04 10:21:48 +0600

    unit test done.

--- backend/spec/factories/destinations.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :destination do
-    name { "Paris" }
-    description { "City of Light" }
-    short_description { "Romantic city" }
-    price { "1200" }
-    img_src { "paris.jpg" }
-  end
-end
